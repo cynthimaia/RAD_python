25cc742d8bb3bb4fb8f6a9dfe9d2b85a52ba86e8
cynthiamaia <cynthiamaia45@gmail.com>
2024-09-10 21:47:11 -0300

    update_

--- banco_dados_python/query.sql
+++
@@ -1,2 +0,0 @@
----SELECT*from Veiculo;---
-SELECT Pessoa.nome, Marca.nome FROM Pessoa JOIN Veiculo ON Pessoa.cpf = Veiculo.Proprietario JOIN Marca on Marca.id = Veiculo.marca WHERE Marca.nome = 'Marca B';
