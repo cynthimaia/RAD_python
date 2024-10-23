6013796c5268298b7f116ba727affd893f54f6da
cynthiamaia <cynthiamaia45@gmail.com>
2024-10-23 07:35:09 -0300

    flask_

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
+#facilita a interação com banco de dados
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db' #URI do banco de dados
+
+db = SQLAlchemy(app)
+#instancia do objeto SQLalchemy
+
+from app.models.products import Products
+with app.app_context(): #contexto da aplicação
+    db.create_all()
 
 @app.route("/index")
 def index():
---
+++ Flask/app/models/products.py
@@ -0,0 +1,34 @@
+from app import db #importei a instancia do SQLALCHEMY(db)
+class Products(db.Model):
+    __tablename__ = 'products'
+    __table_args__ = {'sqlite_autoincrement': True}
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    price = db.Column(db.Float)
+
+    def __init__(self, name, price):
+        #é chamado quando cria uma nova instancia de Products
+        self.name = name
+        self.price = price
+    def save_products(self, name, price):
+        try:
+            add_banco = Products(name, price)
+            #cria uma nova instancia de Products, adicionar no banco de dados
+            db.session.add(add_banco)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+    def update_products(self, id, name, price):
+        try:
+            db.session.query(Products).filter(Products.id == id).update({"name": name, "price": price})
+            db.session.commit()
+        except Exception as e:
+            print(e)
+    def delete_products(self, id):
+        try:
+            db.session.query(Products).filter(Products.id==id).delete()
+            db.session.commit()
+        except Exception as e:
+            print("Não foi possivel excluir os produtos", e)
+
+
